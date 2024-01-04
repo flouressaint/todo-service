2e4f7bcc963b3d7d3959f353e74509b92917cd54
flouressaint <kadomtcev.va@students.dvfu.ru>
2024-01-04 22:09:23 +1000

    added graceful shutdown

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # todo-service
+
+docker-compose up -d   
+go run cmd/app/main.go
--- internal/app/app.go
+++ internal/app/app.go
@@ -2,11 +2,15 @@ package app
 
 import (
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/flouressaint/todo-service/config"
 	"github.com/flouressaint/todo-service/internal/controller"
 	"github.com/flouressaint/todo-service/internal/repo"
 	"github.com/flouressaint/todo-service/internal/service"
+	"github.com/flouressaint/todo-service/pkg/httpserver"
 	"github.com/flouressaint/todo-service/pkg/validator"
 	"github.com/labstack/echo/v4"
 )
@@ -30,5 +34,27 @@ func Run() {
 	handler.Validator = validator.NewCustomValidator()
 	controller.NewRouter(handler, services)
 
-	handler.Start(":" + cfg.ServerPort)
+	// HTTP server
+	log.Printf("Starting http server...")
+	log.Printf("Server port: %s", cfg.ServerPort)
+	httpServer := httpserver.New(handler, httpserver.Port(cfg.ServerPort))
+
+	// Waiting signal
+	log.Printf("Configuring graceful shutdown...")
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case s := <-interrupt:
+		log.Printf("app - Run - signal: " + s.String())
+	case err = <-httpServer.Notify():
+		log.Fatal("app - Run - httpServer.Notify: %w", err)
+	}
+
+	// Graceful shutdown
+	log.Printf("Shutting down...")
+	err = httpServer.Shutdown()
+	if err != nil {
+		log.Fatal("app - Run - httpServer.Shutdown: %w", err)
+	}
 }
